233982179337a37395caaaab7b76e9bfa9b27425
felixcapuano <felix.capuano@gmail.com>
2024-02-09 16:12:17 +0100

    workers endpoints

--- psyncly/api/v1/worker_routes.py
+++ psyncly/api/v1/worker_routes.py
@@ -1,28 +1,64 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from psyncly import schemas, models
-from psyncly.crud.resources_crud import PlaylistCrud, TrackCrud
+from psyncly import models
+from psyncly.crud.resources_crud import WorkerCrud
 from psyncly.dependencies import get_db
+from psyncly.schemas.worker_schemas import Worker, CreateWorker
 
 router = APIRouter(
     tags=["Workers"], prefix="/users/{user_id}/playlists/{playlist_id}/workers"
 )
 
 
-@router.get("")
-async def list_workers(db: Session = Depends(get_db)):
-    return "TODO"
+@router.get("", response_model=list[Worker])
+async def list_workers(
+    user_id: int,
+    playlist_id: int,
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+):
+    return WorkerCrud(db).get(
+        skip=skip,
+        limit=limit,
+        filters=(
+            models.Worker.playlist_id == playlist_id,
+            models.Worker.playlist.owner_id == user_id,
+        ),
+    )
 
 
-@router.get("/{service_playlist_id}")
-async def get_worker(db: Session = Depends(get_db)):
-    return "TODO"
+@router.get("/{worker_id}", response_model=Worker)
+async def get_worker(
+    user_id: int,
+    playlist_id: int,
+    worker_id: int,
+    db: Session = Depends(get_db),
+):
+    worker = WorkerCrud(db).get_by_id(
+        id=worker_id,
+        filters=(
+            models.Worker.playlist_id == playlist_id,
+            models.Worker.playlist.owner_id == user_id,
+        ),
+    )
+    if not worker:
+        raise HTTPException(status_code=404)
+
+    return worker
 
 
-@router.post("")
-async def create_worker(db: Session = Depends(get_db)):
-    return "TODO"
+@router.post("", response_model=Worker)
+async def create_worker(
+    playlist_id: int,
+    worker: CreateWorker,
+    db: Session = Depends(get_db),
+):
+    worker_dict = dict(worker)
+    worker_dict.update(playlist_id=playlist_id)
+
+    return WorkerCrud(db).create(obj=worker_dict)
 
 
 # @router.put("/{service_playlist_id}")
@@ -30,6 +66,17 @@ async def create_worker(db: Session = Depends(get_db)):
 #     return "TODO"
 
 
-@router.delete("/{service_playlist_id}")
-async def delete_worker(db: Session = Depends(get_db)):
-    return "TODO"
+@router.delete("/{worker_id}", status_code=204)
+async def delete_worker(
+    user_id: int,
+    playlist_id: int,
+    worker_id: int,
+    db: Session = Depends(get_db),
+):
+    WorkerCrud(db).delete_by_id(
+        id=worker_id,
+        filters=(
+            models.Worker.playlist_id == playlist_id,
+            models.Worker.playlist.owner_id == user_id,
+        ),
+    )
